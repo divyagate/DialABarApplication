c734c1856c9661adcd654368a2082f13a1a0ff0e
divya <gatedivya123@gmail.com>
2020-11-21 21:03:07 -0500

    login and registration

--- app/src/main/java/com/example/dialabarapplication/LoginActivity.java
+++ app/src/main/java/com/example/dialabarapplication/LoginActivity.java
@@ -1,14 +1,96 @@
 package com.example.dialabarapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText InputEmail, InputPassword;
+    private Button LoginButton;
+    private String parentDbName = "Users";
+    private TextView AdminLink, NotAdminLink;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fAuth= FirebaseAuth.getInstance();
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputEmail = (EditText) findViewById(R.id.login_email_input);
+        AdminLink = (TextView) findViewById(R.id.admin_panel_link);
+        NotAdminLink = (TextView) findViewById(R.id.not_admin_panel_link);
+
+        AdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                LoginButton.setText("Login Admin");
+                AdminLink.setVisibility(View.INVISIBLE);
+                NotAdminLink.setVisibility(View.VISIBLE);
+                parentDbName = "Admins";
+            }
+        });
+
+        NotAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                LoginButton.setText("Login");
+                AdminLink.setVisibility(View.VISIBLE);
+                NotAdminLink.setVisibility(View.INVISIBLE);
+                parentDbName = "Users";
+            }
+        });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String uemail = InputEmail.getText().toString().trim();
+                String upassword = InputPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(uemail)){
+                    InputEmail.setError("Email is required");
+                }
+
+                if(TextUtils.isEmpty(upassword)){
+                    InputPassword.setError("Please enter password");
+                }
+
+                if(upassword.length() < 6){
+                    InputPassword.setError("Password must be more then 6 characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(uemail,upassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"User Logged in", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dialabarapplication/RegisterActivity.java
+++ app/src/main/java/com/example/dialabarapplication/RegisterActivity.java
@@ -1,14 +1,80 @@
 package com.example.dialabarapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Button CreateAccountButton;
+    private EditText InputName, InputEmail, InputPassword;
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        CreateAccountButton = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_username_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputEmail = (EditText) findViewById(R.id.register_email_input);
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            finish();
+        }
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = InputName.getText().toString().trim();
+                String uemail = InputEmail.getText().toString().trim();
+                String upassword = InputPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(uemail)){
+                    InputEmail.setError("Email is required");
+
+                }
+                if(TextUtils.isEmpty(uname)){
+                    InputName.setError("Name is required");
+                }
+                if(TextUtils.isEmpty(upassword)){
+                    InputPassword.setError("Please enter password");
+
+                }
+                if(upassword.length() < 6){
+                    InputPassword.setError("Password must be more then 6 characters");
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(uemail, upassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this,"User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
